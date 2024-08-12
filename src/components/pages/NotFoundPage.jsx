7eef339116cc3309d8cfc3897e08f4f5398b2002
diyorbek <parolxato@gmail.com>
2024-08-12 17:50:07 +0500

    fix: not found page

--- src/components/pages/NotFoundPage.jsx
+++ src/components/pages/NotFoundPage.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NotFoundPage = () => {
     return (
-        <div className="flex flex-col items-center justify-center h-screen bg-gray-light">
+        <div className="flex flex-col items-center justify-center h-96  w-full">
             <h1 className="text-6xl font-bold mb-4">404</h1>
             <p className="text-xl mb-4">Sahifa topilmadi</p>
             <a href="/" className="bg-blue-500 text-white py-2 px-4 rounded">Bosh sahifaga qaytish</a>
